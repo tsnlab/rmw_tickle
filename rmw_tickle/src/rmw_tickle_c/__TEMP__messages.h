5697f00d21bd6b92f6d756f53e9a3883942e8f51
Junho Lee <junho@tsnlab.com>
2026-01-13 15:40:14 +0900

    Initial migration
    Signed-off-by: Junho Lee <junho@tsnlab.com>

---
+++ rmw_tickle/src/rmw_tickle_c/__TEMP__messages.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "rmw_tickle_c/rmw_tickle.h"
+
+struct StringData {
+    char* data;
+};
+
+extern struct tt_Topic StringTopic;
+
+int32_t StringData_encode_size(struct StringData* data);
+int32_t StringData_encode(struct StringData* data, uint8_t* payload, const int32_t len);
+int32_t StringData_decode(struct StringData* data, const uint8_t* payload, const int32_t len, bool is_native_endian);
+void StringData_free(struct StringData* data);
+
+struct HeaderData {
+    struct {
+        int32_t sec;
+        uint32_t nanosec;
+    } stamp;
+    char* frame_id;
+};
+
+extern struct tt_Topic HeaderTopic;
+
+int32_t HeaderData_encode_size(struct HeaderData* data);
+int32_t HeaderData_encode(struct HeaderData* data, uint8_t* payload, const int32_t len);
+int32_t HeaderData_decode(struct HeaderData* data, const uint8_t* payload, const int32_t len, bool is_native_endian);
+void HeaderData_free(struct HeaderData* data);
