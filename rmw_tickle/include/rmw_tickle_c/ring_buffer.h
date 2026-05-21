2a768392dbcbad4f03ea1522d576ef742e7d71b6
Harim Seong <halim9512@gmail.com>
2026-05-21 10:50:19 +0900

    Fix ring buffer compile errors

--- rmw_tickle/include/rmw_tickle_c/ring_buffer.h
+++ rmw_tickle/include/rmw_tickle_c/ring_buffer.h
@@ -7,18 +7,18 @@ extern "C" {
 #endif
 
 struct ring_buffer {
-    void* data;
+    uint8_t* data;
     uint32_t  elem_size;
     uint32_t  capacity;
-    uint32_t  head;
-    uint32_t  tail;
+    uint32_t  write_end;
+    uint32_t  read_end;
 };
 
 struct ring_buffer ring_buffer_create(rcutils_allocator_t allocator, uint32_t elem_size, uint32_t capacity);
 int32_t ring_buffer_init(struct ring_buffer* buffer, rcutils_allocator_t allocator, uint32_t elem_size, uint32_t capacity);
 void ring_buffer_destroy(struct ring_buffer* buffer, rcutils_allocator_t allocator);
 int32_t ring_buffer_push(struct ring_buffer* buffer, void* push_from);
-void* ring_buffer_pop(struct ring_buffer* buffer, void* pop_to);
+int32_t ring_buffer_pop(struct ring_buffer* buffer, void* pop_to);
 uint32_t ring_buffer_size(struct ring_buffer* buffer);
 
 #ifdef __cplusplus
