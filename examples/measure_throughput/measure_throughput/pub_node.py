cb35cf8798a27e3cdb9bba4b44cc5c970e358939
Harim Seong <halim9512@gmail.com>
2026-06-18 15:18:15 +0900

    Improve throughput measurement app
    * Frequency is adjuatable using commandline argument
    * Replace 64-bit payload data unit with 8-bit
    * Add logger callback to separate logging from listener callback

--- examples/measure_throughput/measure_throughput/pub_node.py
+++ examples/measure_throughput/measure_throughput/pub_node.py
@@ -4,26 +4,36 @@ import array
 from rclpy.node import Node
 from my_message.msg import Payload
 
+# subscriber receive rate is around 2000~3000 messages per second. 
+# 10000 ensures maximum throughput
+DEFAULT_FREQUENCY = 10000
+
 class ThroughputPublisher(Node):
-    def __init__(self, payload_size):
+    def __init__(self, payload_size, frequency):
         super().__init__('throughput_publisher')
         self.publisher_ = self.create_publisher(Payload, 'throughput_topic', 1)
-        self.timer = self.create_timer(0.00001, self.timer_callback) # 100,000Hz
+        self.timer = self.create_timer(1 / frequency, self.timer_callback)
         self.payload_size = payload_size
         self.get_logger().info("Publisher Node started. Sending timestamps...")
 
     def timer_callback(self):
         msg = Payload()
-        msg.payload = array.array('Q', [0xAAAAAAAAAAAAAAAA] * (self.payload_size // 8))
+        msg.payload = array.array('B', [0xAA] * (self.payload_size))
         self.publisher_.publish(msg)
 
 def main(args=sys.argv):
-    if len(args) != 2:
-        print(f"usage:  ros2 run measure_throughput pub payload_size")
-        print(f"        payload size is not given")
+    if len(args) < 2 or len(args) > 3:
+        print(f"usage:  ros2 run measure_throughput pub payload_size [frequency]")
+        print(f"        payload_size    payload size in bytes")
+        print(f"        frequency       sending frequency in Hz. Default value is 10,000")
         return
+    payload_size = int(args[1])
+    if len(args) == 3:
+        frequency = float(args[2])
+    else:
+        frequency = DEFAULT_FREQUENCY
     rclpy.init(args=None)
-    node = ThroughputPublisher(int(args[1]))
+    node = ThroughputPublisher(payload_size, frequency)
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
--- examples/measure_throughput/measure_throughput/sub_node.py
+++ examples/measure_throughput/measure_throughput/sub_node.py
@@ -18,42 +18,37 @@ class ThroughputLogger(Node):
         self.file_exists = os.path.isfile(self.filename)
         self.csv_file = open(self.filename, mode='a', newline='')
         self.writer = csv.writer(self.csv_file)
+        self.logger_timer = self.create_timer(1, self.logger_callback)
         
         # Write header only if the file is created
         if not self.file_exists:
             self.writer.writerow(['elapsed_second', 'message_per_second', 'throughput_mbps'])
         
-        self.current_second = self.get_clock().now().seconds_nanoseconds()[0]
         self.total_throughput_sum = 0
         self.throughput_sum_per_second = 0
         self.elapsed_second = 1
         self.message_count = 0
         self.get_logger().info(f"Logging throughput to {self.filename}")
 
+    def logger_callback(self):
+        throughput_avg = self.total_throughput_sum / self.elapsed_second
+        self.get_logger().info(f"Throughput: {self.throughput_sum_per_second / 1000000:.4f} Mbps | Message/second: {self.message_count} | Avg: {throughput_avg / 1000000:.4f} Mbps")
+        self.writer.writerow([self.elapsed_second, self.message_count, self.throughput_sum_per_second / 1000000])
+        self.throughput_sum_per_second = 0
+        self.message_count = 0
+        self.elapsed_second += 1
+
     def listener_callback(self, msg: Payload):
         arr = msg.payload
-        # length of array = number of 8 byte element
-        # length of payload in bits = 8 * 8 * length of array
-        msg_size_bytes = len(arr) * 8
-        msg_size_bits = msg_size_bytes * 8
+        msg_size_bits = len(arr) * 8
 
         for elem in arr:
-            if elem != 0xAAAAAAAAAAAAAAAA:
+            if elem != 0xAA:
                 self.get_logger().warning("invalid data")
 
         self.total_throughput_sum += msg_size_bits
         self.throughput_sum_per_second += msg_size_bits
         self.message_count += 1
-        current_second = self.get_clock().now().seconds_nanoseconds()[0]
-        # Log to CSV every second
-        if current_second > self.current_second:
-            self.current_second = current_second
-            throughput_avg = self.total_throughput_sum / self.elapsed_second
-            self.get_logger().info(f"Size: {msg_size_bytes} Bytes | Throughput: {self.throughput_sum_per_second / 1000000:.4f} Mbps | Message/second: {self.message_count} | Avg: {throughput_avg / 1000000:.4f} Mbps")
-            self.writer.writerow([self.elapsed_second, self.message_count, self.throughput_sum_per_second / 1000000])
-            self.throughput_sum_per_second = 0
-            self.message_count = 0
-            self.elapsed_second += 1
 
     def destroy_node(self):
         self.csv_file.close()
